c6d05d3cde9b2fe8566c81c64c803cf30b544d8f
Xskyis <nabilafgarezaafgarza@gmail.com>
2023-11-08 09:53:32 +0700

    first commit

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { useState, useContext } from 'react'
 import GithubContext from '../../context/github/GithubContext'
+import AlertContext from '../../context/alert/AlertContext'
+import { searchUsers } from '../../context/github/GithubAction'
 
 function UserSearch () {
   const [text, setText] = useState('')
 
-  const { users, searchUsers } = useContext(GithubContext)
+  const { users, dispatch } = useContext(GithubContext)
+  const { setAlert } = useContext(AlertContext)
 
   const handleChange = e => setText(e.target.value)
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
 
     if (text === '') {
-      alert('Please enter something!')
+      setAlert('Please enter something!', 'error')
     } else {
-      searchUsers(text)
+      dispatch({ type: 'SET_LOADING' })
+      const users = await searchUsers(text)
+      dispatch({ type: 'GET_USERS', payload: users })
+
       setText('')
     }
   }
@@ -35,7 +41,7 @@ function UserSearch () {
               />
               <button
                 type='submit'
-                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+                className='absolute top-0 right-0 rounded-l-none w-40 btn btn-lg'
               >
                 Go
               </button>
@@ -45,7 +51,12 @@ function UserSearch () {
       </div>
       {users.length > 0 && (
         <div>
-          <button className='btn btn-ghost btn-lg'>Clear</button>
+          <button
+            className='btn btn-ghost btn-lg'
+            onClick={() => dispatch({ type: 'CLEAR_USERS' })}
+          >
+            Clear
+          </button>
         </div>
       )}
     </div>
